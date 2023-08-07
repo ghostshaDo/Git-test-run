90533ddd3d20d1e583bd953033e400582d8a825e
Viranchee Joshi <43431727+ghostshaDo@users.noreply.github.com>
2023-08-07 18:45:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-var item = document.getElementById('items');
-console.log(item);
-console.log(item[1]);
-item[1].textContent = 'Hello ';
+var titles = document.querySelector('.title');
 
-
-
-
-
-
-
-
+var odd = document.querySelectorAll('lo:nth-child(odd)');
+odd[1].style.backgorngColor = "Green";
+odd[2].style.textColor = "white";
